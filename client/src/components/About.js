7573c53988bba571f5c8c914df4b171317aaa72b
akashdas99 <akashdas0599@gmail.com>
2021-01-27 21:14:26 +0530

    added about

---
+++ client/src/components/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className="neo card about">
+            <div className="about-header"><span style={{ color: "#2196f3", fontWeight: "bold" }} >UN</span>doubt </div>
+            <div className="about-body">
+                <p>A Website where Your Questions meets others Answers.</p>
+                <p>Post your Questions and get your Doubts removed by others.</p>
+                <p>Answer other's Question and remove their Doubts.</p>
+                <p>It's "Better to ask a Question than to remain Ignorant".</p>
+            </div>
+        </div>
+    )
+}
+
+export default About
