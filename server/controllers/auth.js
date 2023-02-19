814cd98d86902b59ddecad8ccdec712aa8378ac7
Akash <akashdas0599@example.com>
2023-02-19 21:45:00 +0530

    Replaced email with username

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,78 +1,75 @@
-const bcrypt = require('bcrypt');
-const User = require('../models/user');
-const jwt = require('jsonwebtoken');
-const expressJwt = require('express-jwt');
+const bcrypt = require("bcrypt");
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+const expressJwt = require("express-jwt");
 
 exports.signup = (req, res) => {
-    //if user exists
-    User.findOne({ email: req.body.email }, (err, user) => {
-        if (user) {
-            return res.status(400).json({
-                error: "User exists"
-            })
-        }
-        if (err)
-            return res.status(400).json({
-                error: err
-            })
-    })
-    const user = new User(req.body)
+  //if user exists
+  User.findOne({ username: req.body.username }, (err, user) => {
+    if (user) {
+      return res.status(400).json({
+        error: "Username exists",
+      });
+    }
+    if (err)
+      return res.status(400).json({
+        error: err,
+      });
+  });
+  const user = new User(req.body);
 
-    bcrypt.hash(req.body.password, 10, (err, hash) => {
-        if (err) {
-            return res.status(400).json({
-                error: "Unable to hash password"
-            })
-        }
-        user.password = hash;
-        user.save((err, user) => {
-            if (err) {
-                return res.status(400).json({
-                    error: "Unable to save user"
-                })
-            }
-            res.json({ name: user.name, email: user.email })
-        })
-    })
+  bcrypt.hash(req.body.password, 10, (err, hash) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Unable to hash password",
+      });
+    }
+    user.password = hash;
+    user.save((err, user) => {
+      if (err) {
+        return res.status(400).json({
+          error: "Unable to save user",
+        });
+      }
+      res.json({ name: user.name, username: user.username });
+    });
+  });
+};
+exports.signin = (req, res) => {
+  //todo compare
 
-
-}
-exports.signin = (req, res) => {//todo compare
-    User.findOne({ email: req.body.email }, (err, user) => {
-        if (err || !user) {
-            return res.status(400).json({
-                error: "User doesnot exists"
-            })
-        }
-        bcrypt.compare(req.body.password, user.password, (err, result) => {
-            if (err || !result) {
-                return res.status(401).json({
-                    error: "Incorrect password"
-                })
-            }
-            const token = jwt.sign({ _id: user._id }, process.env.SECRET);
-            return res.json({ token: token, userId: user._id });
-        })
-
-    })
-
-}
+  User.findOne({ username: req.body.username }, (err, user) => {
+    if (err || !user) {
+      return res.status(400).json({
+        error: "User doesnot exists",
+      });
+    }
+    bcrypt.compare(req.body.password, user.password, (err, result) => {
+      if (err || !result) {
+        return res.status(401).json({
+          error: "Incorrect password",
+        });
+      }
+      const token = jwt.sign({ _id: user._id }, process.env.SECRET);
+      return res.json({ token: token, userId: user._id });
+    });
+  });
+};
 exports.signout = (req, res) => {
-    return res.json("Signed out");
-}
+  return res.json("Signed out");
+};
 
 exports.isSignedIn = expressJwt({
-    secret: process.env.SECRET,
-    algorithms: ['HS256'],
-    userProperty: 'auth'
-
-})
+  secret: process.env.SECRET,
+  algorithms: ["HS256"],
+  userProperty: "auth",
+});
 exports.isAuthorized = (req, res, next) => {
-    let checker = req.profile && req.auth && req.profile._id == req.auth._id;
-    if (!checker) {
-        return res.status(403).json({
-            error: "Access Denied",
-        });
-    }
-    next();
-}
+  let checker = req.profile && req.auth && req.profile._id == req.auth._id;
+  if (!checker) {
+    return res.status(403).json({
+      error: "Access Denied",
+    });
+  }
+  next();
+};
--- server/models/user.js
+++ server/models/user.js
@@ -1,42 +1,42 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
 
 //user schema
 const userSchema = mongoose.Schema(
-    {
-        //name of the user
-        name: {
-            type: String,
-            maxlength: 40,
-            trim: true,
-            required: true,
-        },
-        email: {
-            type: String,
-            trim: true,
-            required: true,
-            unique: true
-        },
-        password: {
-            type: String,
-            required: true,
-        },
-        profession: {
-            type: String,
-            required: true,
-            trim: true
-        },
-        city: {
-            type: String,
-            required: true,
-            trim: true
-        },
-        country: {
-            type: String,
-            required: true,
-            trim: true
-        }
-    }, { timestamps: true }
-)
+  {
+    //name of the user
+    name: {
+      type: String,
+      maxlength: 40,
+      trim: true,
+      required: true,
+    },
+    username: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profession: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    city: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    country: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model("User", userSchema)
+module.exports = mongoose.model("User", userSchema);
