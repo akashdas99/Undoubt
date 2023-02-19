4a9a9cc9742f09d80d2d47c3be6dbbd25483c064
Akash <akashdas0599@example.com>
2023-02-19 21:45:28 +0530

    Replaced email with username

--- client/src/components/UserCard.js
+++ client/src/components/UserCard.js
@@ -1,76 +1,71 @@
-import { Edit } from '@material-ui/icons'
-import React, { useEffect, useState } from 'react'
-import { isAuthenticated } from '../services/authapihelper'
-import { userinfo } from '../services/userapihelper'
-import UpdateUser from './UpdateUser'
-
+import { Edit } from "@material-ui/icons";
+import React, { useEffect, useState } from "react";
+import { isAuthenticated } from "../services/authapihelper";
+import { userinfo } from "../services/userapihelper";
+import UpdateUser from "./UpdateUser";
 
 function UserCard({ user }) {
-    const [value, setvalue] = useState({
-        name: "",
-        email: "",
-        city: "",
-        country: "",
-        registered: "",
-        profession: "",
-    })
+  const [value, setvalue] = useState({
+    name: "",
+    username: "",
+    city: "",
+    country: "",
+    registered: "",
+    profession: "",
+  });
 
-    const [editState, seteditState] = useState(false)
-    const { userId } = isAuthenticated()
+  const [editState, seteditState] = useState(false);
+  const { userId } = isAuthenticated();
 
-    const toggleEdit = () => {
-        seteditState(!editState);
-    }
+  const toggleEdit = () => {
+    seteditState(!editState);
+  };
 
-    const loaduser = () => {
-        seteditState(false);
-        userinfo(user).then(data => {
-            if (data.error) {
-                console.log(data.error);
-                setvalue({ ...value, errors: data.error })
-            }
-            else {
-                setvalue(
-                    {
-                        ...value,
-                        name: data.name,
-                        email: data.email,
-                        profession: data.profession,
-                        city: data.city,
-                        country: data.country,
-                        registered: new Date(data.createdAt).toLocaleDateString(),
-                    }
-                )
-            }
-        })
-    }
+  const loaduser = () => {
+    seteditState(false);
+    userinfo(user).then((data) => {
+      if (data.error) {
+        console.log(data.error);
+        setvalue({ ...value, errors: data.error });
+      } else {
+        setvalue({
+          ...value,
+          name: data.name,
+          username: data.username,
+          profession: data.profession,
+          city: data.city,
+          country: data.country,
+          registered: new Date(data.createdAt).toLocaleDateString(),
+        });
+      }
+    });
+  };
 
-    const { name, email, city, country, registered, profession } = value;
-    useEffect(() => {
-        loaduser();
-    }, [user])
-    return (
-        <>
-            {!editState && (<div className="neo user-info">
-                {/* {loading && } */}
+  const { name, username, city, country, registered, profession } = value;
+  useEffect(() => {
+    loaduser();
+  }, [user]);
+  return (
+    <>
+      {!editState && (
+        <div className="neo user-info">
+          {/* {loading && } */}
 
-                <div className="user-name">{name}</div>
-                <span>{user === userId && <Edit onClick={toggleEdit} />}</span>
-                <div>Email  -  {email}</div>
-                <div>Profession - {profession}</div>
-                <div>Lives in -  {city},{country}</div>
-                <div>Registered  -  {registered}</div>
-
-            </div>)}
-            {editState && (
-                <UpdateUser
-                    userInfo={value}
-                    reload={loaduser}
-                    cancel={toggleEdit}
-                />)
-            }
-        </>
-    )
+          <div className="user-name">{name}</div>
+          <span>{user === userId && <Edit onClick={toggleEdit} />}</span>
+          <div>Username - {username}</div>
+          <div>Profession - {profession}</div>
+          <div>
+            Lives in - {city},{country}
+          </div>
+          <div>Registered - {registered}</div>
+        </div>
+      )}
+      {editState && (
+        <UpdateUser userInfo={value} reload={loaduser} cancel={toggleEdit} />
+      )}
+    </>
+  );
 }
 
-export default UserCard
+export default UserCard;
